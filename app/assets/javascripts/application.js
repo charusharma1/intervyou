ef331e14ca003feef94647f1a5e0e654780c7d57
ACLTC <acltc@ACLTCs-MacBook-Air-8.local>
2015-12-09 19:13:32 -0800

    istalled theme umarket

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,43 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require theme/bootstrap-hover-dropdown.min
+//= require theme/bootstrap.min
+//= require theme/color-switcher
+//= require theme/custom
+//= require theme/jquery.card
+//= require theme/jquery.dragtable
+//= require theme/owl.carousel.min
+//= require theme/SmoothScroll
+//= require theme/twitterFetcher_min
+//= require theme/bootstrap-hover-dropdown.min
+//= require theme/bootstrap.min
+//= require theme/color-switcher
+//= require theme/custom
+//= require theme/jquery.card
+//= require theme/jquery.dragtable
+//= require theme/owl.carousel.min
+//= require theme/SmoothScroll
+//= require theme/twitterFetcher_min
+//= require theme/bootstrap-hover-dropdown.min
+//= require theme/bootstrap.min
+//= require theme/color-switcher
+//= require theme/custom
+//= require theme/jquery.card
+//= require theme/jquery.dragtable
+//= require theme/owl.carousel.min
+//= require theme/SmoothScroll
+//= require theme/twitterFetcher_min
+//= require_tree .
